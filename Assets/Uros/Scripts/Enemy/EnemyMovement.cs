0ef91333b29e4b277b0fc753ff78683d13bdda3d
Uros <urosspasenovic9@gmail.com>
2022-07-16 18:55:28 +0200

    Enemy movement with serialized field move speed

--- Assets/Uros/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Uros/Scripts/Enemy/EnemyMovement.cs
@@ -5,6 +5,9 @@ using DG.Tweening;
 
 public class EnemyMovement : MonoBehaviour
 {
+    [SerializeField]
+    float moveSpeed = 1f;
+
     bool canMove = true;
     Waypoint waypoint;
     Rigidbody rigidbody;
@@ -27,22 +30,17 @@ public class EnemyMovement : MonoBehaviour
     {
         int randomNode;
         thisNode = GraphBuilder.Graph.Find(waypoint);
-        print("nodes " +thisNode.Neighbors.Count);
         if(thisNode.Neighbors.Count <= 3)
         {
             randomNode = Random.Range(0, thisNode.Neighbors.Count);
-            print("kranji " + randomNode);
         }
         else
         {
             int random1 = Random.Range(0, thisNode.Neighbors.Count / 2);
-            print("1  " + random1);
             int random2 = Random.Range(thisNode.Neighbors.Count / 2, thisNode.Neighbors.Count);
-            print("2  " + random2);
             int[] rand = { random1, random2 };
             int randIndex = Random.Range(0,2);
             randomNode = rand[randIndex];
-            print("krajnji 2 " + randomNode);
         }
         
         GoToTarget(randomNode);
@@ -51,9 +49,8 @@ public class EnemyMovement : MonoBehaviour
     private void GoToTarget(int randomNode)
     {
         Waypoint targetWaypoint = thisNode.Neighbors[randomNode].Value;
-        print(targetWaypoint.Id);
         canMove = false;
-        transform.DOMove(targetWaypoint.transform.position, 1f).OnComplete(() => NextTargert());
+        transform.DOMove(targetWaypoint.transform.position, moveSpeed).OnComplete(() => NextTargert());
     }
 
     private void NextTargert()
